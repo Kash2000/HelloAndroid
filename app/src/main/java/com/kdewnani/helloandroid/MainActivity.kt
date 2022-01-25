f8147501d98522a91cc4c844c7e2fc145eb62efe
Kash2000 <kashishgdewnani@gmail.com>
2022-01-24 22:34:04 -0500

    Second commit!

--- app/src/main/java/com/kdewnani/helloandroid/MainActivity.kt
+++ app/src/main/java/com/kdewnani/helloandroid/MainActivity.kt
@@ -13,7 +13,7 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main;
+        setContentView(R.layout.activity_main);
 
     }
 }
